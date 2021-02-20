2ea2c3bfe602a52a942db8149586ee03840497e4
leo <69598134+Leeeeeeeeeeeeeo@users.noreply.github.com>
2021-02-20 15:37:11 -0500

    no ingredient

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients' 
 
 const burger = (props) => {
-  const transformedIngredients =Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
+        console.log(i)
         return <BurgerIngredient key={igKey + i} type={igKey} />
       });
-    });
+    }) .reduce((arr,el) => {
+      return arr.concat(el)
+    },[])
+    if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+
   return (
     <div className = {classes.Burger}>
       <BurgerIngredient type = "bread-top" />
