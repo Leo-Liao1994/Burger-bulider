ac6a3e3b7b9dbbb349d33ebd2f2b1f86492860c7
leo <69598134+Leeeeeeeeeeeeeo@users.noreply.github.com>
2021-03-09 16:32:09 -0500

    backdrop

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,8 +1,12 @@
 import React from "react";
 import classes from './Modal.css'
+import Aux from '../../../hoc/Aux'
+import Backdrop from '../Backdrop/Backdrop'
 
 const modal = (props) => {
   return (
+    <Aux>
+      <Backdrop show = {props.show} clicked = {props.modalClosed}/>
     <div 
     className = {classes.Modal}
      style = {{
@@ -11,6 +15,7 @@ const modal = (props) => {
      }}>
       {props.children} 
     </div>
+    </Aux>
   )
 };
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -65,7 +65,11 @@ class BurgerBuilder extends Component {
   }
 
   purchaseHandler = () => {
-    this.setState({purchasing:true})
+    this.setState({purchasing:true});
+  }
+
+  purchaseCancelHandler = () => {
+    this.setState({purchasing:false});
   }
 
   render () {
@@ -78,7 +82,7 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal show ={this.state.purchasing}>
+        <Modal show ={this.state.purchasing} modalClosed = {this.purchaseCancelHandler}>
           <OrderSummary ingredients={this.state.ingredients}/>
         </Modal>
         <Burger ingredients={this.state.ingredients}/>
